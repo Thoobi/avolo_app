c9cc4c6822f3df73353d7ccdab22fcd317cf8347
thoobi <sydneydaniel76@gmail.com>
2026-07-05 02:31:28 +0100

    chores: changes content for the profile content

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,8 +2,13 @@ import { View, Text } from 'react-native';
 
 export default function Profile() {
   return (
-    <View className="flex-1 bg-yellow-400">
-      <Text className="text-3xl text-black">This is the Profile</Text>
+    <View className="flex-1 bg-white px-4 pt-6">
+      <View className="pb-6">
+        <Text className="text-xl font-bold text-black">Profile</Text>
+        <Text className="mt-1 text-sm text-muted">
+          I will see if there is need to add a profile and auth screen
+        </Text>
+      </View>
     </View>
   );
 }
